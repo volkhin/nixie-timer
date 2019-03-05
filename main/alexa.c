057d213f82101595f1b9ac040a551c9352894821
Artem Volkhin <volkhin@fb.com>
2019-03-04 23:57:50 -0800

    display timer on LCD screen

--- main/alexa.c
+++ main/alexa.c
@@ -12,7 +12,9 @@
 #include "alexaGadgetStateListenerStateUpdateDirective.pb.h"
 #include "btstack_debug.h"
 #include "directiveParser.pb.h"
+#include "esp_heap_caps.h"
 #include "esp_log.h"
+#include "esp_system.h"
 #include "eventParser.pb.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -20,12 +22,18 @@
 #include "pb.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
+#include "spiffs_vfs.h"
+#include "tft.h"
+#include "tftspi.h"
 
 static const char* TAG = "alexa.c";
 
 uint8_t out_buffer[1000];
 uint16_t out_buffer_len;
 static time_t timer;
+
+// Define which spi bus to use TFT_VSPI_HOST or TFT_HSPI_HOST
+#define SPI_BUS TFT_HSPI_HOST
 
 void create_discovery_response() {
   /*
@@ -129,7 +137,7 @@ void create_discovery_response() {
   bool status = pb_encode(
       &stream, alexaDiscovery_DiscoverResponseEventProto_fields, &env);
   if (!status) {
-    ESP_LOGE(TAG, "Error encoding message");
+    ESP_LOGE(TAG, "Error encoding message: %s", PB_GET_ERROR(&stream));
     return;
   }
 
@@ -262,9 +270,81 @@ void register_send_data_callback(send_data_callback_t callback) {
 
 void timer_task(void* pvParameter) {
   ESP_LOGI(TAG, "timer_task started");
+  esp_err_t ret;
+
+  tft_disp_type = DEFAULT_DISP_TYPE;
+  _width = DEFAULT_TFT_DISPLAY_WIDTH;    // smaller dimension
+  _height = DEFAULT_TFT_DISPLAY_HEIGHT;  // larger dimension
+  max_rdclock = 8000000;
+
+  // === Pins MUST be initialized before SPI interface initialization ===
+  TFT_PinsInit();
+
+  spi_lobo_device_handle_t spi;
+  spi_lobo_bus_config_t buscfg = {
+      .miso_io_num = PIN_NUM_MISO,  // set SPI MISO pin
+      .mosi_io_num = PIN_NUM_MOSI,  // set SPI MOSI pin
+      .sclk_io_num = PIN_NUM_CLK,   // set SPI CLK pin
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+      .max_transfer_sz = 6 * 1024,
+  };
+  spi_lobo_device_interface_config_t devcfg = {
+      .clock_speed_hz = 8000000,          // Initial clock out at 8 MHz
+      .mode = 0,                          // SPI mode 0
+      .spics_io_num = -1,                 // we will use external CS pin
+      .spics_ext_io_num = PIN_NUM_CS,     // external CS pin
+      .flags = LB_SPI_DEVICE_HALFDUPLEX,  // ALWAYS SET  to HALF DUPLEX MODE!!
+                                          // for display spi
+  };
+
+  vTaskDelay(500 / portTICK_RATE_MS);
+
+  ESP_LOGI("SPI", "Pins used: miso=%d, mosi=%d, sck=%d, cs=%d", PIN_NUM_MISO,
+           PIN_NUM_MOSI, PIN_NUM_CLK, PIN_NUM_CS);
+
+  // ==== Initialize the SPI bus and attach the LCD to the SPI bus ====
+  ret = spi_lobo_bus_add_device(SPI_BUS, &buscfg, &devcfg, &spi);
+  assert(ret == ESP_OK);
+  ESP_ERROR_CHECK(ret);
+  ESP_LOGI("SPI", "display device added to spi bus (%d)", SPI_BUS);
+  disp_spi = spi;
+
+  // ==== Test select/deselect ====
+  ret = spi_lobo_device_select(spi, 1);
+  ESP_ERROR_CHECK(ret);
+  ret = spi_lobo_device_deselect(spi);
+  ESP_ERROR_CHECK(ret);
+
+  ESP_LOGI("SPI", "attached display device, speed=%u", spi_lobo_get_speed(spi));
+  ESP_LOGI("SPI", "bus uses native pins: %s",
+           spi_lobo_uses_native_pins(spi) ? "true" : "false");
+
+  // ==== Initialize the Display ====
+  ESP_LOGI("SPI", "display init...");
+  TFT_display_init();
+  ESP_LOGI("SPI", "ok");
+
+  // ---- Detect maximum read speed ----
+  max_rdclock = find_rd_speed();
+  ESP_LOGI("SPI", "Max rd speed = %u", max_rdclock);
+
+  // ==== Set SPI clock used for display operations ====
+  spi_lobo_set_speed(spi, DEFAULT_SPI_CLOCK);
+  ESP_LOGI("SPI", "Changed speed to %u", spi_lobo_get_speed(spi));
+
+  TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+  TFT_setRotation(PORTRAIT);
+  TFT_setFont(DEFAULT_FONT, NULL);
+  TFT_resetclipwin();
+
+  TFT_fillScreen(TFT_BLACK);
+  TFT_print("Hello world!", CENTER, CENTER);
+
   for (;;) {
     static struct timeval now;
     gettimeofday(&now, NULL);
+    static char timer_output[10];
     if (timer != 0 && now.tv_sec != 0) {
       time_t time_left = timer - now.tv_sec;
       if (time_left < 0) {
@@ -272,10 +352,13 @@ void timer_task(void* pvParameter) {
       } else if (time_left > 99 * 60 + 99) {
         time_left = 99 * 60 + 99;
       }
-      ESP_LOGI("TIMER", "%02ld:%02ld", time_left / 60, time_left % 60);
+      sprintf(timer_output, "%02ld:%02ld", time_left / 60, time_left % 60);
     } else {
-      ESP_LOGI("TIMER", "<blank>");
+      sprintf(timer_output, "<blank>");
     }
+    ESP_LOGI("TIMER", "%s", timer_output);
+    TFT_fillScreen(TFT_BLACK);
+    TFT_print(timer_output, CENTER, CENTER);
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
--- main/alexa.h
+++ main/alexa.h
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-#define TIMER_TASK_STACK_SIZE 4096
+#define TIMER_TASK_STACK_SIZE 16<<10
 
 typedef void (*send_data_callback_t)(uint8_t* buffer, uint16_t len);
 
